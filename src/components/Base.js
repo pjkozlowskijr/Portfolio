88f60fb8a1e2153e3c9ca396e3f55188a88086f5
Patrick Kozlowski <pjkozlowskijr@gmail.com>
2022-09-13 11:23:07 -0400

    npm installs and create needed components

---
+++ src/components/Base.js
@@ -0,0 +1,9 @@
+import About from "./About";
+import Contact from "./Contact";
+import Navbar from "./Navbar";
+import Projects from "./Projects";
+import Skills from "./Skills";
+
+export default function Base() {
+    
+}
