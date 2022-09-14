ca61176d5e4a5ac9f8de0a6b03c6960211ddd74d
Patrick Kozlowski <pjkozlowskijr@gmail.com>
2022-09-14 10:20:03 -0400

    Work on layout of home page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 export default function Home() {
     return(
-        <div className='container min-h-full'>
-            <h1 className='text-5xl'>Patrick Kozlowski</h1>
-            <h2 className='text-4xl'>Builds cool stuff with code.</h2>
-            <p>I'm a Full Stack Software Engineer based in Indianapolis, IN, who enjoys solving complex problems and bringing ideas to life through code. I appreciate you taking time to visit my profile and getting to know more about me. If you like my work, please reach out so we can connect!</p>
+        <div className='columns-2'>
+            <div className='h-screen'>
+                <h1 className='text-5xl'>Patrick Kozlowski</h1>
+                <h2 className='text-4xl'>Builds cool stuff with code.</h2>
+                <p>I'm a Full Stack Software Engineer based in Indianapolis, IN, who enjoys solving complex problems and bringing ideas to life through code. I appreciate you taking time to visit my profile and getting to know more about me. If you like my work, please reach out so we can connect!</p>
+            </div>
+            <div className='h-screen'>
+                <img src='https://res.cloudinary.com/detcvmtip/image/upload/v1663161879/Photo_-_Patrick_Kozlowski_dzmztz.png' alt='Patrick Kozlowski' className='h-1/2'/>
+            </div>
         </div>
     )
 }
