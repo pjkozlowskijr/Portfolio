344c13434491a2ae562aa5da18e8726b6d1c35d9
Patrick Kozlowski <pjkozlowskijr@gmail.com>
2022-09-26 11:06:46 -0400

    Add skills to About page.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -16,13 +16,90 @@ export default function Skills() {
                 <span className={styles.terminalText}>$ ls</span>
             </p>
             <br/>
-            <ul className={styles.terminalText}>
-                <li>Python</li>
-                <li>React</li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
+            <div className={styles.skills}>
+                <ul className={styles.terminalText}>
+                    <li>
+                        <img className={styles.transBack} src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/react/react-original-wordmark.svg" alt="React" height="40" width="40"/>
+                        &nbsp;React.js
+                    </li>
+                    <li>
+                        <img className={styles.transBack} src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/javascript/javascript-original.svg" alt="JavaScript" height="40" width="40"/>
+                        &nbsp;JavaScript
+                    </li>
+                    <li>
+                        <img className={styles.transBack} src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/html5/html5-original-wordmark.svg" alt="HTML5" height="40" width="40"/>
+                        &nbsp;HTML5
+                    </li>
+                    <li>
+                        <img className={styles.transBack} src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/css3/css3-original-wordmark.svg" alt="CSS3" height="40" width="40"/>
+                        &nbsp;CSS3
+                    </li>
+                    <li>
+                        <img className={styles.transBack} src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/materialui/materialui-original.svg" alt="MUI" height="40" width="40"/>
+                        &nbsp;Material UI
+                    </li>
+                    <li>
+                        <img className={styles.transBack} src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/bootstrap/bootstrap-original-wordmark.svg" alt="Bootstrap" height="40" width="40"/>
+                        &nbsp;Bootstrap
+                    </li>
+                    <li>
+                        <img className={styles.transBack} src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/postgresql/postgresql-original-wordmark.svg" alt="PostgreSQL" height="40" width="40"/>
+                        &nbsp;PostgreSQL
+                    </li>
+                </ul>
+                <ul className={styles.terminalText}>
+                    <li>
+                        <img className={styles.transBack} src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/sqlite/sqlite-original-wordmark.svg" alt="SQLite" height="40" width="40"/>
+                        &nbsp;SQLite
+                    </li>
+                    <li>
+                        <img className={styles.transBack} src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/python/python-original-wordmark.svg" alt="Python" height="40" width="40"/>
+                        &nbsp;Python
+                    </li>
+                    <li>
+                        <img className={styles.transBack} src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/flask/flask-original-wordmark.svg" alt="Flask" height="40" width="40"/>
+                        &nbsp;Flask
+                    </li>
+                    <li>
+                        <img className={styles.transBack} src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/sqlalchemy/sqlalchemy-original-wordmark.svg" alt="SQLAlchemy" height="40" width="40"/>
+                        &nbsp;SQLAlchemy
+                    </li>
+                    <li>
+                        <img className={styles.transBack} src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/linux/linux-original.svg" alt="Linux" height="40" width="40"/>
+                        &nbsp;Linux
+                    </li>
+                    <li>
+                        <img className={styles.transBack} src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/git/git-original-wordmark.svg" alt="Git" height="40" width="40"/>
+                        &nbsp;Git
+                    </li>
+                </ul>
+                <ul className={styles.terminalText}>
+                    <li>
+                        <img className={styles.transBack} src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/github/github-original-wordmark.svg" alt="GitHub" height="40" width="40"/>
+                        &nbsp;GitHub
+                    </li>
+                    <li>
+                        <img className={styles.transBack} src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/heroku/heroku-original-wordmark.svg" alt="Heroku" height="40" width="40"/>
+                        &nbsp;Heroku
+                    </li>
+                    <li>
+                        <img className={styles.transBack} src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/vscode/vscode-original-wordmark.svg" alt="VS Code" height="40" width="40"/>
+                        &nbsp;VS Code
+                    </li>
+                    <li>
+                        <img className={styles.transBack} src="https://res.cloudinary.com/detcvmtip/image/upload/v1664203769/api_1_yfbecm.png" alt="RESTful API" height="40" width="40"/>
+                        &nbsp;RESTful API
+                    </li>
+                    <li>
+                        <img className={styles.transBack} src="https://res.cloudinary.com/detcvmtip/image/upload/v1664203847/oop_ydkywe.png" alt="Object-Oriented Programming" height="40" width="40"/>
+                        &nbsp;Object-Oriented Programming
+                    </li>
+                    <li>
+                        <img className={styles.transBack} src="https://res.cloudinary.com/detcvmtip/image/upload/v1664204000/agile_wygdql.png" alt="Agile" height="40" width="40"/>
+                        &nbsp;Agile &#40;Scrum&#41;
+                    </li>
+                </ul>
+            </div>
         </>
     )
 }
