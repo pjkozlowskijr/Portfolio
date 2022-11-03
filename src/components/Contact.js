3b4b9ca5982f143f7c93b2537f326184a19a76f1
Patrick Kozlowski <pjkozlowskijr@gmail.com>
2022-11-03 10:45:42 -0400

    Connect 'Contact' page to email service & create functioning form.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,23 @@
+import { useRef } from 'react';
 import EmailIcon from '@mui/icons-material/Email';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
+    const form = useRef();
+    const contact_number = Math.random() * 100000 | 0;
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm('default_service', 'contact_form', form.current, 'KlBx0GIMp8GUxNj1I')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
     return(
         <div className='container flex flex-row'>
             <div className='basis-6/12'>
@@ -17,24 +32,27 @@ export default function Contact() {
                 </div>
             </div>
             <div className='basis-6/12'>
-                <form>
+                <form ref={form} onSubmit={sendEmail}>
+                    <input type="hidden" name="contact_number" defaultValue={contact_number}></input>
                     <table>
-                        <tr>
-                            <td><label for="name"><strong>Name: </strong></label></td>
-                            <td><input type="text" id="name" placeholder="Your name..."></input></td>
-                        </tr>
-                        <tr>
-                            <td><label for="email"><strong>Email: </strong></label></td>
-                            <td><input type="email" id="email" placeholder="Your email..."></input></td>
-                        </tr>
-                        <tr>
-                            <td><label for="message"><strong>Message: </strong></label></td>
-                            <td><textarea id="message" placeholder="Your message..."></textarea></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td><input id="submit" type="submit" value="Submit"></input></td>
-                        </tr>
+                        <tbody>
+                            <tr>
+                                <td><label htmlFor="user_name"><strong>Name: </strong></label></td>
+                                <td><input type="text" id="user_name" name="user_name" placeholder="Your name..."></input></td>
+                            </tr>
+                            <tr>
+                                <td><label htmlFor="user_email"><strong>Email: </strong></label></td>
+                                <td><input type="email" id="user_email" name="user_email" placeholder="Your email..."></input></td>
+                            </tr>
+                            <tr>
+                                <td><label htmlFor="message"><strong>Message: </strong></label></td>
+                                <td><textarea id="message" name="message" placeholder="Your message..."></textarea></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td><input id="submit" type="submit" value="Submit"></input></td>
+                            </tr>
+                        </tbody>
                     </table>
                 </form>
             </div>
